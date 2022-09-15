5aaf6729737d3a1108bc962387fb93930a27e99e
Xalageus <xalageus@gmail.com>
2022-09-14 21:42:20 -0500

    fixed tabbing

--- libvirt_remote_manager/utils.py
+++ libvirt_remote_manager/utils.py
@@ -1,7 +1,7 @@
 import os
 
 def check_if_root() -> bool:
-        if os.getuid() == 0:
-            return True
-        else:
-            return False
+    if os.getuid() == 0:
+        return True
+    else:
+        return False
