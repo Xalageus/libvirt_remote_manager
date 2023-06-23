542ad67e34a0cf4415b9a0618442fb56c6859d3a
Xalageus <xalageus@gmail.com>
2023-06-22 20:47:51 -0500

    use log decorator instead

--- libvirt_remote_manager/pom_server.py
+++ libvirt_remote_manager/pom_server.py
@@ -3,22 +3,23 @@ from libvirt_remote_manager import app
 from flask import request, abort
 from libvirt_remote_manager._response_data import ResultBool
 from libvirt_remote_manager.host_power import HostPower
+from libvirt_remote_manager.server_dec import debug_log_url
 
 @app.route('/api/host/pom/shutdown', methods=['POST'])
+@debug_log_url
 def host_shutdown():
-    logging.debug(request.url)
     hp = HostPower()
     done = hp.host_shutdown()
     return ResultBool(done, '').toJSON()
 
 @app.route('/api/host/pom/reboot', methods=['POST'])
+@debug_log_url
 def host_reboot():
-    logging.debug(request.url)
     hp = HostPower()
     done = hp.host_reboot()
     return ResultBool(done, '').toJSON()
 
 @app.route('/api/host/pom/ping')
+@debug_log_url
 def ping():
-    logging.debug(request.url)
     return "pong"
